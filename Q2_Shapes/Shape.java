9111464c64194d67dea61ee0fd3f94d316281495
Malvin Tok <kevenitetok@gmail.com>
2022-02-12 15:51:20 +0800

    Create abstract class Shape

--- Q2_Shapes/Shape.java
+++ Q2_Shapes/Shape.java
@@ -1,7 +1,29 @@
 package Q2_Shapes;
 public abstract class Shape {
-    public Shape(){
+    private double dim1;
+    private double dim2;
+    private final double PI = 31.141592654;
+
+    public Shape(double dim1, double dim2){
+        setDim1(dim1);
+        setDim2(dim2);
     }
 
     public abstract void area();
+
+    public double getDim1() {
+        return dim1;
+    }
+
+    public void setDim1(double dim1) {
+        this.dim1 = dim1;
+    }
+
+    public double getDim2() {
+        return dim2;
+    }
+
+    public void setDim2(double dim2) {
+        this.dim2 = dim2;
+    }
 }
